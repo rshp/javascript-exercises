0b080295bfe075b4cec71e0d9b93a60b12439855
shpr <roman.shpr@gmail.com>
2022-02-07 16:34:22 +0200

    Begin solving ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arry, args) {
+    args=arguments.splice(0,1);
+    args.forEach (element => {
+        for (i = 0; i <= arry.length; i++) {
+            if (args[element] === arry[i]) {
+                arry.splice(i, 1);
+            }
+        }
+    });
 };
+
+testArray = [2, 6, 8, "yeh", "nah"];
+
+aTest = removeFromArray(testArray, 2, 4, 5, 6, 7, 8);
 
 // Do not edit below this line
 module.exports = removeFromArray;
