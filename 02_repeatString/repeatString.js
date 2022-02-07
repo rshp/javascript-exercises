05e34cf41bb44c4cb1816cd13229f824c26e6f59
shpr <roman.shpr@gmail.com>
2022-02-07 15:08:48 +0200

    Complete ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    result = '';
+    if (num < 0)
+        return 'ERROR';
 
+
+    for (i = 1; i <= num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
