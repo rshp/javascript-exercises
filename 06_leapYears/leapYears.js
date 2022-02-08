50cf31a0e427a830490fcae8ce293135b833dc95
shpr <roman.shpr@gmail.com>
2022-02-08 08:49:28 +0200

    Add ex 06 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (!(year % 100) && !(year % 400)) return true
+    else if (!(year%100)) return false
+    else if (!(year % 4)) return true
+    else return false
 };
 
 // Do not edit below this line
