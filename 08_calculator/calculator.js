edda4a651643a74295584d691c1904905591ed1f
shpr <roman.shpr@gmail.com>
2022-02-22 22:07:41 +0200

    Partial solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
