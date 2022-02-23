eda21a8810c111d5d40f6ae53b1e58dcda971096
shpr <roman.shpr@gmail.com>
2022-02-23 09:56:14 +0200

    Add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	string = string
+		.toLowerCase(string)
+		.match(/[a-z]+/g)
+		.join('');
+	reversed = Array.from(string).reverse().join('');
+	if (string === reversed) return true;
+	else return false;
 };
 
 // Do not edit below this line
