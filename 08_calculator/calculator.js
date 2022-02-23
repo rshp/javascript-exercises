34ae640de09dd1a25b4faf5d5bcbe9e679ba16e5
shpr <roman.shpr@gmail.com>
2022-02-23 08:14:35 +0200

    Add calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,13 +6,29 @@ const subtract = function (a, b) {
 	return a - b;
 };
 
-const sum = function () {};
+const sum = function (list) {
+	return list.reduce((total, num) => (total += num), 0);
+};
 
-const multiply = function () {};
+const multiply = function (list) {
+	return list.reduce((total, num) => (total *= num), 1);
+};
 
-const power = function () {};
+const power = function (a, b) {
+	return Math.pow(a, b);
+};
 
-const factorial = function () {};
+const factorial = function (num) {
+	if (num === 0) return 1;
+	else {
+		let res = 1;
+		while (num > 0) {
+			res *= num;
+			num -= 1;
+		}
+		return res;
+	}
+};
 
 // Do not edit below this line
 module.exports = {
