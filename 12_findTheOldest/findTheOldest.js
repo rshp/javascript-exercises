c8e3b0ef73abec36a4781022a611ddd3f2bc6fa8
shpr <roman.shpr@gmail.com>
2022-02-23 16:21:04 +0200

    Add partial solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const stuff = [
+	{
+		name: 'Carly',
+		yearOfBirth: 2018,
+	},
+	{
+		name: 'Ray',
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: 'Jane',
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+];
 
+const findTheOldest = function (people) {
+	let ages = people.map((person) => {
+		if (!person.yearOfDeath) {
+			return [person.name, new Date().getFullYear() - person.yearOfBirth];
+		} else {
+			return [person.name, person.yearOfDeath - person.yearOfBirth];
+		}
+	});
+
+	console.table(ages);
 };
-
+findTheOldest(stuff);
 // Do not edit below this line
 module.exports = findTheOldest;
