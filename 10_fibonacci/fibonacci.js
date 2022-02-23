c682511b8eff85981a8f0c30bb3fccfaef2a0c3a
shpr <roman.shpr@gmail.com>
2022-02-23 11:50:22 +0200

    Add ex10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	if (num < 0) return 'OOPS';
+	else if (num == 1 || num == 2) return 1;
+	else {
+		let prevNum1 = 1;
+		let prevNum2 = 1;
+		for (let index = 3; index <= num; index++) {
+			var currentNum = prevNum1 + prevNum2;
+			prevNum1 = prevNum2;
+			prevNum2 = currentNum;
+		}
+		return currentNum;
+	}
 };
 
 // Do not edit below this line
